ef44f3d0fa85ea1279b1b0bd59b8697064b2fece
hyein7447 <hyein7447@gmail.com>
2024-01-15 18:14:35 +0900

    네이버로그인

---
+++ naver/script/login.js
@@ -0,0 +1,35 @@
+// 로그인 탭 제목 클릭시 해당 내용 활성화 JS
+// 0. (초기) 로그인내용 O, 일회용내용X, QR코드내용X, ID로그인 제목 활성화 (css)
+// 1. ID로그인을 클릭하면 로그인내용 O, 일회용내용X, QR코드내용X
+// 2. 일회용을 클릭하면 로그인내용 X, 일회용내용O, QR코드내용X
+// 3. QR코드를 클릭하면 로그인내용 X, 일회용내용X, QR코드내용O
+// 제목 변수
+const login_title = document.querySelectorAll('.login_title h2 > a')
+// 내용 변수
+const id_login_container = document.querySelector('.login_container .id_login')
+const disposable_login_container = document.querySelector('.login_container .disposable_login')
+const qr_login_container = document.querySelector('.login_container .qr_login')
+console.log(id_login_container, disposable_login_container, qr_login_container)
+
+// 초기 일회용 , qr 내용 숨기기
+disposable_login_container.computedStyleMap.display = 'none'
+qr_login_container.style.display = 'none'
+
+// 일회용을 클릭하면 로그인내용 X, 일회용내용O, QR코드내용X
+login_title[1].addEventListener('click', ()=> {
+    disposable_login_container.style.display = 'block'
+    id_login_container.style.display = 'none'
+    qr_login_container.style.display = 'none'
+})
+// ID로그인을 클릭하면 로그인내용 O, 일회용내용X, QR코드내용X
+login_title[0].addEventListener('click',()=>{
+    disposable_login_container.style.display = 'none'
+    qr_login_container.style.display = 'none'
+    id_login_container.style.display = 'block'
+})
+// QR코드를 클릭하면 로그인내용 X, 일회용내용X, QR코드내용O
+login_title[2].addEventListener('click',()=>{
+    id_login_container.style.display = 'none'
+    disposable_login_container.style.display = 'none'
+    qr_login_container.style.display = 'block'
+})
